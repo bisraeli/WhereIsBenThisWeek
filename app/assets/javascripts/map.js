50beb84f2600e7c3c3d8c335f1392b6d64440611
Jason Rogers <jason.rogers@samasource.org>
2013-09-06 01:40:47 -0400

    staggering markers
    - make markers with same lat/long be staggered on the page so that you can see each day of the week
    - cleaned up some UI bits on event listing, like truncating the external ID
    - changed how events are loaded (Ajax pulled now so that we use the index.json.jbuilder template)

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,38 +1,29 @@
 var Colors = {
-  0: 'FF00FF',
-  1: '00FF00',
-  2: '0000FF',
-  3: 'FFFF00',
-  4: 'FF9900',
-  5: 'FF0000',
-  6: 'DDD'
-};
-
-var DaysOfWeek = {
-  0: 'Sunday',
-  1: 'Monday',
-  2: 'Tuesday',
-  3: 'Wednesday',
-  4: 'Thursday',
-  5: 'Friday',
-  6: 'Saturday'
+  'Sunday': 'FF00FF',
+  'Monday': '00FF00',
+  'Tuesday': '0000FF',
+  'Wednesday': 'FFFF00',
+  'Thursday': 'FF9900',
+  'Friday': 'FF0000',
+  'Saturday': 'DDD'
 };
 
 var map;
-var oms;
-var iw;
+var overlappingMarkerSpiderfier;
+var markerInfoWindow;
+var allLocations = {};
+
 var createLegend = function() {
   var legendContainer = $("#map-legend");
   var list = $('<ul></ul>');
   legendContainer.append(list);
-  for (var i in Colors) {
-    list.append(legendItem(i));
+  for (var dayOfWeek in Colors) {
+    list.append(legendItem(dayOfWeek));
   }
 };
 
-var legendItem = function(index) {
-  var pinImageSrc = pinImageSource(Colors[index]);
-  var dayOfWeek = DaysOfWeek[index];
+var legendItem = function(dayOfWeek) {
+  var pinImageSrc = pinImageSource(Colors[dayOfWeek]);
   return $('<li><img src="' +
     pinImageSrc +
     '" altText="' +
@@ -44,19 +35,19 @@ var legendItem = function(index) {
 
 var createMap = function(lat, lon) {
   var mapOptions = {
-    center: new google.maps.LatLng(40.754084,-73.966448),
+    center: new google.maps.LatLng(42.3488218, -83.0593771),
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map($("#map")[0],mapOptions);
-  oms = new OverlappingMarkerSpiderfier(map);
-  iw  = new google.maps.InfoWindow();
-  oms.addListener('hover', function(marker, event) {
-    iw.setContent(marker.desc);
-    iw.open(map, marker);
+  overlappingMarkerSpiderfier = new OverlappingMarkerSpiderfier(map);
+  markerInfoWindow  = new google.maps.InfoWindow();
+  overlappingMarkerSpiderfier.addListener('hover', function(marker, event) {
+    markerInfoWindow.setContent(marker.desc);
+    markerInfoWindow.open(map, marker);
   });
-  oms.addListener('spiderfy', function(markers) {
-    iw.close();
+  overlappingMarkerSpiderfier.addListener('spiderfy', function(markers) {
+    markerInfoWindow.close();
   });
 };
 
@@ -65,34 +56,33 @@ var pinImageSource = function(pinColor) {
 }
 
 var createEventMarker = function(event) {
-  console.log(event.start_time);
-  var pinColor = Colors[(new Date(event.start_time)).getDay()];
-  var pinImage = new google.maps.MarkerImage(pinImageSource(pinColor),
-        new google.maps.Size(21, 34),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(10, 34));
-  var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
-        new google.maps.Size(40, 37),
-        new google.maps.Point(5, 5),
-        new google.maps.Point(12, 35));
+  var locationKey = event.latitude + "," + event.longitude;
+  if (!allLocations.hasOwnProperty(locationKey)) {
+    allLocations[locationKey] = 0;
+  } else {
+    allLocations[locationKey] = allLocations[locationKey] + 0.001;
+  }
 
+  var pinColor = Colors[event.day_of_week];
   var markerOptions = {
-    position: new google.maps.LatLng(event.latitude, event.longitude),
+    position: new google.maps.LatLng(event.latitude + allLocations[locationKey], event.longitude),
     visible: true,
     clickable: true,
-    //draggable: true,
-    icon: pinImage,
-    // shadow: pinShadow,
+    // draggable: true,
+    icon: {
+      url: pinImageSource(pinColor),
+      size: new google.maps.Size(21, 34),
+      anchor: google.maps.Point(0, 0)
+    },
     map: map,
-    title: event.client//event.start_time + "-" + event.end_time
+    title: event.client + "\nFrom " + event.start_time + " to " + event.end_time
   };
 
   var marker = new google.maps.Marker(markerOptions);
   marker.desc = event.client;
-  oms.addMarker(marker);
+  overlappingMarkerSpiderfier.addMarker(marker);
   google.maps.event.addListener(marker,'click',function(){
     showEventInfo(event, marker);
-    // window.location.href = '/jams/start';
   });
 };
 
